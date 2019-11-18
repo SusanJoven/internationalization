a892358f18b609dbff464e3e96525e77e057fa6b
SusanJoven <sp.joven@uniandes.edu.co>
2019-11-18 13:47:19 -0500

    Cambios

--- src/components/job.js
+++ src/components/job.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import {FormattedDate} from 'react-intl';
+import {FormattedNumber} from 'react-intl';
+import {FormattedPlural} from 'react-intl';
+
+
+
+
 
 export default class Job extends React.Component {
 
@@ -8,10 +15,27 @@ export default class Job extends React.Component {
   				<th scope="row">{this.props.offer.id}</th>
       			<td>{this.props.offer.name}</td>
   				<td>{this.props.offer.company}</td>
-      			<td>{this.props.offer.salary}</td>
+      			<td>				  {this.props.offer.salary}
+
+				  <FormattedPlural value={this.props.offer.salary} one=" millon" other=" millons" />
+				</td>
       			<td>{this.props.offer.city}</td>
-      			<td>{this.props.offer.date}</td>
+				<td>
+				  <FormattedDate 
+				  	value={new Date(this.props.offer.date)}
+                    year='numeric'
+                    month='long'
+                    day='numeric'
+                    weekday='long'
+                  />
+                </td>
+                <td>
+                  <FormattedNumber 
+                   value={this.props.offer.views} />
+                </td>
   			</tr>
+
+
   		);
 	}
 }
