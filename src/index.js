b1dac4135915b8d83a830a263e62831b174f54ff
SusanJoven <sp.joven@uniandes.edu.co>
2019-11-18 22:42:44 -0500

    Cambios

--- src/index.js
+++ src/index.js
@@ -2,20 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {IntlProvider} from 'react-intl';
 import localeEsMessages from "./locales/es";
-//import localeEnMessages from "./locales/en";
-
-
-
+import localeEnMessages from "./locales/en";
 import JobsList from "./components/jobsList";
 
 
 
+let getBrowserLanguage = ()=> {
+    return navigator.language || navigator.userLanguage;
+}
+let getLanguage=()=>{
+	var lan = getBrowserLanguage();
+	if(lan == 'es-ES'){
+		return "es";
+	}
+	return "en";
 
 
+}
+let getLanguageData=()=>{
+	var lan = getBrowserLanguage();
+	if (lan == 'es-ES'){
+		return localeEsMessages;
+	}
+	return localeEnMessages;
+
+}
 
 
 ReactDOM.render(
-	<IntlProvider locale='es-ES' messages={localeEsMessages}>
+	<IntlProvider locale={getLanguage()} messages={getLanguageData()}>
 		<JobsList/>
 	</IntlProvider>, document.getElementById("root")
 );
